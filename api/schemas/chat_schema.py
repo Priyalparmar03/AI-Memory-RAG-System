6bd7654ad9a18b3a70b2e438b9f17d3d02afd2be
Priyal parmar <parmarpriyal1603@gmail.com>
2026-07-24 22:57:38 +0530

    Update chat_schema.py

--- api/schemas/chat_schema.py
+++ api/schemas/chat_schema.py
@@ -1,11 +1,3 @@
-"""
-app/schemas/chat_schema.py
-
-Chat Schemas
-
-Author: Priyal Parmar
-"""
-
 from __future__ import annotations
 
 from typing import List, Optional
@@ -77,5 +69,3 @@ class ChatHistory(BaseModel):
     session_id: str
 
     messages: List[ChatMessage]
-
-
